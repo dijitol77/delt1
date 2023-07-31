dcbec8a8b3a1d5864c00e6dc90c470b271a1a63e
dijitol77 <saul.a.howells@gmail.com>
2023-07-31 08:09:35 +0100

    Update PluginEditor.cpp

--- src/PluginEditor.cpp
+++ src/PluginEditor.cpp
@@ -38,22 +38,20 @@ addAndMakeVisible(modelSelect3);
 modelSelect3.setColour(juce::Label::textColourId, juce::Colours::black);
 modelSelect3.setScrollWheelEnabled(true);
 int c = 1;
-for (const auto& jsonFile : processor.jsonFiles) {
-    modelSelect3.addItem(jsonFile.getFileName(), c);
-    c += 1;
-}
-modelSelect3.onChange = [this] {modelSelect3Changed();};
+    for (const auto& jsonFile : processor.jsonFiles) {
+        modelSelect3.addItem(jsonFile.getFileName(), c);
+        c += 1;
+    }
+    modelSelect3.onChange = [this] { modelSelect3Changed(); };
 
-addAndMakeVisible(modelSelect4);
-modelSelect4.setColour(juce::Label::textColourId, juce::Colours::black);
-
-modelSelect4.setScrollWheelEnabled(true);
-c = 1;
-for (const auto& jsonFile : processor.jsonFiles) {
-    modelSelect4.addItem(jsonFile.getFileName(), c);
-    c += 1;
-}
-modelSelect4.onChange = [this] {modelSelect4Changed();};
+    modelSelect4.setColour(juce::Label::textColourId, juce::Colours::black);
+    modelSelect4.setScrollWheelEnabled(true);
+    c = 1; // Remove this line as c is already defined above
+    for (const auto& jsonFile : processor.jsonFiles) {
+        modelSelect4.addItem(jsonFile.getFileName(), c);
+        c += 1;
+    }
+    modelSelect4.onChange = [this] { modelSelect4Changed(); };
 
     addAndMakeVisible(loadButton2);
 loadButton2.setButtonText("LOAD MODEL 2");
