5437fbb79395f670225297636608ca8ead5ed498
dijitol77 <saul.a.howells@gmail.com>
2023-08-02 23:40:42 +0100

    Update PluginProcessorInit.h

--- src/PluginProcessorInit.h
+++ src/PluginProcessorInit.h
@@ -2,15 +2,3 @@
 
 #include "JuceHeader.h"
 #include "PluginProcessor.h"
-
-class ProteusAudioProcessor : public AudioProcessor
-{
-public:
-    ProteusAudioProcessor();
-    ~ProteusAudioProcessor();
-
-    // Other member function declarations...
-
-private:
-    // Private member variables and functions...
-};
