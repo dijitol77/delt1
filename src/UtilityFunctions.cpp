892af36ab00c9559e1e303b5df12d8b94803c807
dijitol77 <saul.a.howells@gmail.com>
2023-08-02 17:38:35 +0100

    Update UtilityFunctions.cpp

--- src/UtilityFunctions.cpp
+++ src/UtilityFunctions.cpp
@@ -1,11 +1,5 @@
 #include "PluginProcessor.h"
 
-void ProteusAudioProcessor::set_ampEQ(float bass_slider, float mid_slider, float treble_slider)
-{
-    eq4band.setParameters(bass_slider, mid_slider, treble_slider, 0.0f);
-    eq4band2.setParameters(bass_slider, mid_slider, treble_slider, 0.0f);
-}
-
 void ProteusAudioProcessor::loadConfig(File configFile)
 {
     this->suspendProcessing(true);
