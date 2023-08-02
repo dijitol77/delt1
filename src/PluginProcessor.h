5a39ddce81e697808502a49e621c5864893f2248
dijitol77 <saul.a.howells@gmail.com>
2023-08-02 21:00:33 +0100

    Update PluginProcessor.h

--- src/PluginProcessor.h
+++ src/PluginProcessor.h
@@ -1,17 +1,6 @@
-/*
-  ==============================================================================
-
-    This file was auto-generated!
-
-    It contains the basic framework code for a JUCE plugin processor.
-
-  ==============================================================================
-*/
-
 #pragma once
-
+#include "AudioProcessing.h"
 #include "../JuceLibraryCode/JuceHeader.h"
-
 
 #define GAIN_ID "drive"
 #define GAIN_NAME "Drive"
