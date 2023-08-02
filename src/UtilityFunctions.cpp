2060c3aec544b6fa37e807f2dcb141ff991db2ad
dijitol77 <saul.a.howells@gmail.com>
2023-08-02 12:44:17 +0100

    Update UtilityFunctions.cpp

--- src/UtilityFunctions.cpp
+++ src/UtilityFunctions.cpp
@@ -2,10 +2,30 @@
 
 void ProteusAudioProcessor::set_ampEQ(float bass_slider, float mid_slider, float treble_slider)
 {
-    // set_ampEQ implementation...
+    eq4band.setParameters(bass_slider, mid_slider, treble_slider, 0.0f);
+    eq4band2.setParameters(bass_slider, mid_slider, treble_slider, 0.0f);
 }
 
 void ProteusAudioProcessor::loadConfig(File configFile)
 {
-    // loadConfig implementation...
+    this->suspendProcessing(true);
+    pauseVolume = 3;
+    String path = configFile.getFullPathName();
+    char_filename = path.toUTF8();
+
+    LSTM.reset();
+    LSTM2.reset();
+
+    LSTM.load_json(char_filename);
+    LSTM2.load_json(char_filename);
+
+    if (LSTM.input_size == 1) {
+        conditioned = false;
+    } else {
+        conditioned = true;
+    }
+
+    //saved_model = configFile;
+    model_loaded = true;
+    this->suspendProcessing(false);
 }
