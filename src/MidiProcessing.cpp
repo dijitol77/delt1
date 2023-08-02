c4c3802998b0c7d06bfcdc327d196c0ba80c12fc
dijitol77 <saul.a.howells@gmail.com>
2023-08-02 12:17:23 +0100

    Create MidiProcessing.cpp

---
+++ src/MidiProcessing.cpp
@@ -0,0 +1,28 @@
+#include "PluginProcessor.h"
+
+bool ProteusAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool ProteusAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool ProteusAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
