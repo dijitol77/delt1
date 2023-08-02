bf3b0658b5cc5982d3e32b1c3694856aff4fb780
dijitol77 <saul.a.howells@gmail.com>
2023-08-02 23:02:39 +0100

    Update Programs.h

--- src/Programs.h
+++ src/Programs.h
@@ -1,7 +1,27 @@
-#pragma once
-#include "AudioProcessing.h"
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+#include "Programs.h"
+#include "juce_core/juce_core.h" // Include this for the String class
 
-class ProteusAudioProcessor;
-class Programs {
-    // Your class definition for Programs
-};
+int ProteusAudioProcessor::getNumPrograms()
+{
+    return 1;
+}
+
+int ProteusAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void ProteusAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const juce::String ProteusAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void ProteusAudioProcessor::changeProgramName (int index, const juce::String& newName)
+{
+}
