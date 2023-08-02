9c57721fe1457f947b3e11517dd04ce8fd8ec26b
dijitol77 <saul.a.howells@gmail.com>
2023-08-02 14:55:37 +0100

    Update PluginProcessor.cpp

--- src/PluginProcessor.cpp
+++ src/PluginProcessor.cpp
@@ -1,3 +1,5 @@
+#include <juce_audio_processors/juce_audio_processors.h> // for AudioBuffer, MidiBuffer
+#include <juce_dsp/juce_dsp.h> // for dsp::AudioBlock, dsp::ProcessContextReplacing, ScopedNoDenormals
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 #include "PluginProcessorInit.cpp"
