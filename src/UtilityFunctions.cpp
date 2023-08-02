d469ac8ccde39931f50b273be2635e647849df31
dijitol77 <saul.a.howells@gmail.com>
2023-08-02 12:12:18 +0100

    Create UtilityFunctions.cpp

---
+++ src/UtilityFunctions.cpp
@@ -0,0 +1,11 @@
+#include "PluginProcessor.h"
+
+void ProteusAudioProcessor::set_ampEQ(float bass_slider, float mid_slider, float treble_slider)
+{
+    // set_ampEQ implementation...
+}
+
+void ProteusAudioProcessor::loadConfig(File configFile)
+{
+    // loadConfig implementation...
+}
