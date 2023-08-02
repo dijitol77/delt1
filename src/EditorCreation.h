612c8477638dc7e5a64ec81fc6f9c968ff519f85
dijitol77 <saul.a.howells@gmail.com>
2023-08-02 22:58:32 +0100

    Update EditorCreation.h

--- src/EditorCreation.h
+++ src/EditorCreation.h
@@ -1,7 +1,11 @@
 #pragma once
 #include "AudioProcessing.h"
+#include "juce_audio_processors/juce_audio_processors.h" // Include this for the AudioProcessorEditor class
 
 class ProteusAudioProcessor;
+
 class EditorCreation {
-    // Your class definition for EditorCreation
+public:
+    bool hasEditor() const;
+    juce::AudioProcessorEditor* createEditor();
 };
