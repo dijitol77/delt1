5634adc34a06ac829260d43f9dbaf2043615f87c
dijitol77 <saul.a.howells@gmail.com>
2023-08-02 19:48:59 +0100

    Update AudioProcessing.h

--- src/AudioProcessing.h
+++ src/AudioProcessing.h
@@ -1,11 +1,27 @@
 #pragma once
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
+#ifndef AUDIO_PROCESSING_H_INCLUDED
+#define AUDIO_PROCESSING_H_INCLUDED
 
-class ProteusAudioProcessor
-{
+#include <juce_audio_processors/juce_audio_processors.h>
+
+class ProteusAudioProcessor : public juce::AudioProcessor {
 public:
-    void prepareToPlay (double sampleRate, int samplesPerBlock);
-    void releaseResources();
-    void processBlock (AudioBuffer<float>& buffer, MidiBuffer& midiMessages);
+    // Constructor, destructor, and other public methods
+
+    void prepareToPlay(double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+    void processBlock(juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages) override;
+
+private:
+    // Private member variables and objects
+    juce::IIRFilter dcBlocker;
+    juce::ResamplingAudioSource resampler;
+    juce::LSTM LSTM;
+    juce::LSTM LSTM2;
+    juce::Convolution cabSimIRa;
+    // ... other member variables ...
 };
+
+#endif // AUDIO_PROCESSING_H_INCLUDED
