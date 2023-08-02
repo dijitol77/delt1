7733109a263cb249d5e46a0a8de817a133f2d8be
dijitol77 <saul.a.howells@gmail.com>
2023-08-02 23:00:40 +0100

    Update StateManagement.h

--- src/StateManagement.h
+++ src/StateManagement.h
@@ -1,7 +1,11 @@
 #pragma once
 #include "AudioProcessing.h"
+#include "juce_core/juce_core.h" // Include this for the MemoryBlock class
 
 class ProteusAudioProcessor;
+
 class StateManagement {
-    // Your class definition for StateManagement
+public:
+    void getStateInformation (juce::MemoryBlock& destData);
+    void setStateInformation (const void* data, int sizeInBytes);
 };
