b1da04f14208a4dac16640d2114ed983a0c6f551
dijitol77 <saul.a.howells@gmail.com>
2023-08-02 12:16:00 +0100

    Create StateManagement.cpp

---
+++ src/StateManagement.cpp
@@ -0,0 +1,11 @@
+#include "PluginProcessor.h"
+
+void ProteusAudioProcessor::getStateInformation (MemoryBlock& destData)
+{
+    // getStateInformation implementation...
+}
+
+void ProteusAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // setStateInformation implementation...
+}
