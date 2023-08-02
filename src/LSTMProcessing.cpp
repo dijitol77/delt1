5b7e3a6ebfacf5243e65a5d4e784e208781e5c20
dijitol77 <saul.a.howells@gmail.com>
2023-08-02 12:10:33 +0100

    Delete LSTMProcessing.cpp

--- src/LSTMProcessing.cpp
+++
@@ -1,7 +0,0 @@
-#include "LSTMProcessing.h"
-
-void processWithLSTM(RT_LSTM& lstm, float* channelData, int numSamples) {
-    for (int i = 0; i < numSamples; ++i) {
-        channelData[i] = lstm.forward(channelData[i]);
-    }
-}
