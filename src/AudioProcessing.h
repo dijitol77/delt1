8627589ac6c07c56394d6ae12fbb7030188b2bd5
dijitol77 <saul.a.howells@gmail.com>
2023-08-02 20:34:53 +0100

    Update AudioProcessing.h

--- src/AudioProcessing.h
+++ src/AudioProcessing.h
@@ -1,10 +1,12 @@
 #pragma once
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
+
 #ifndef AUDIO_PROCESSING_H_INCLUDED
 #define AUDIO_PROCESSING_H_INCLUDED
 
 #include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_dsp/juce_dsp.h> // Include this for dsp related classes
 
 class ProteusAudioProcessor : public juce::AudioProcessor {
 public:
@@ -18,9 +20,9 @@ private:
     // Private member variables and objects
     juce::IIRFilter dcBlocker;
     juce::ResamplingAudioSource resampler;
-    juce::LSTM LSTM;
-    juce::LSTM LSTM2;
-    juce::Convolution cabSimIRa;
+    juce::dsp::LSTM LSTM; // Using dsp::LSTM from juce_dsp namespace
+    juce::dsp::LSTM LSTM2; // Using dsp::LSTM from juce_dsp namespace
+    juce::dsp::Convolution cabSimIRa; // Using dsp::Convolution from juce_dsp namespace
     // ... other member variables ...
 };
 
