71fea0a0cd121f6963da99c6fe2da989a3010f66
dijitol77 <saul.a.howells@gmail.com>
2023-08-02 23:41:33 +0100

    Update PluginProcessorInit.cpp

--- src/PluginProcessorInit.cpp
+++ src/PluginProcessorInit.cpp
@@ -1,7 +1,7 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
+#include <juce_audio_processors/juce_audio_processors.h> // for AudioParameterFloat
 
-//==============================================================================
 ProteusAudioProcessor::ProteusAudioProcessor()
 #ifndef JucePlugin_PreferredChannelConfigurations
     : AudioProcessor(BusesProperties()
@@ -12,14 +12,11 @@ ProteusAudioProcessor::ProteusAudioProcessor()
         .withOutput("Output", AudioChannelSet::stereo(), true)
 #endif
     ),
-
     treeState(*this, nullptr, "PARAMETER", { std::make_unique<AudioParameterFloat>(GAIN_ID, GAIN_NAME, NormalisableRange<float>(0.0f, 1.0f, 0.01f), 0.5f),
                         std::make_unique<AudioParameterFloat>(BASS_ID, BASS_NAME, NormalisableRange<float>(-8.0f, 8.0f, 0.01f), 0.0f),
                         std::make_unique<AudioParameterFloat>(MID_ID, MID_NAME, NormalisableRange<float>(-8.0f, 8.0f, 0.01f), 0.0f),
                         std::make_unique<AudioParameterFloat>(TREBLE_ID, TREBLE_NAME, NormalisableRange<float>(-8.0f, 8.0f, 0.01f), 0.0f),
                         std::make_unique<AudioParameterFloat>(MASTER_ID, MASTER_NAME, NormalisableRange<float>(0.0f, 1.0f, 0.01f), 0.5) })
-
-    
 #endif
 {
     driveParam = treeState.getRawParameterValue (GAIN_ID);
@@ -38,47 +35,8 @@ ProteusAudioProcessor::ProteusAudioProcessor()
     pauseVolume = 3;
 
     cabSimIRa.load(BinaryData::default_ir_wav, BinaryData::default_ir_wavSize);
-
 }
 
 ProteusAudioProcessor::~ProteusAudioProcessor()
 {
 }
-
-//==============================================================================
-const String ProteusAudioProcessor::getName() const
-{
-    return JucePlugin_Name;
-}
-
-double ProteusAudioProcessor::getTailLengthSeconds() const
-{
-    return 0.0;
-}
-
-//==============================================================================
-
-
-#ifndef JucePlugin_PreferredChannelConfigurations
-bool ProteusAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
-{
-  #if JucePlugin_IsMidiEffect
-    ignoreUnused (layouts);
-    return true;
-  #else
-    // This is the place where you check if the layout is supported.
-    // In this template code we only support mono or stereo.
-    if (layouts.getMainOutputChannelSet() != AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != AudioChannelSet::stereo())
-        return false;
-
-    // This checks if the input layout matches the output layout
-   #if ! JucePlugin_IsSynth
-    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
-        return false;
-   #endif
-
-    return true;
-  #endif
-}
-#endif
