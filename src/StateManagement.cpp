082086b38d221e4cb6b697ba7b640d508c83e07a
dijitol77 <saul.a.howells@gmail.com>
2023-08-02 17:13:56 +0100

    Update StateManagement.cpp

--- src/StateManagement.cpp
+++ src/StateManagement.cpp
@@ -48,39 +48,6 @@ void ProteusAudioProcessor::setStateInformation (const void* data, int sizeInByt
         }
     }
 }
-
-void ProteusAudioProcessor::set_ampEQ(float bass_slider, float mid_slider, float treble_slider)
-{
-    eq4band.setParameters(bass_slider, mid_slider, treble_slider, 0.0f);
-    eq4band2.setParameters(bass_slider, mid_slider, treble_slider, 0.0f);
-}
-
-void ProteusAudioProcessor::loadConfig(File configFile)
-{
-    this->suspendProcessing(true);
-    pauseVolume = 3;
-    String path = configFile.getFullPathName();
-    char_filename = path.toUTF8();
-
-    LSTM.reset();
-    LSTM2.reset();
-
-    LSTM.load_json(char_filename);
-    LSTM2.load_json(char_filename);
-
-    if (LSTM.input_size == 1) {
-        conditioned = false;
-    } else {
-        conditioned = true;
-    }
-
-    //saved_model = configFile;
-    model_loaded = true;
-    this->suspendProcessing(false);
-}
-
-
-
 //==============================================================================
 // This creates new instances of the plugin..
 AudioProcessor* JUCE_CALLTYPE createPluginFilter()
