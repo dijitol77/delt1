ab017fa66dfbf9fa29d6f763e2433362a602f217
dijitol77 <saul.a.howells@gmail.com>
2023-08-02 12:18:02 +0100

    Create AudioProcessing.cpp

---
+++ src/AudioProcessing.cpp
@@ -0,0 +1,11 @@
+#include "PluginProcessor.h"
+
+void ProteusAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    // prepareToPlay implementation...
+}
+
+void ProteusAudioProcessor::processBlock (AudioBuffer<float>& buffer, MidiBuffer& midiMessages)
+{
+    // processBlock implementation...
+}
