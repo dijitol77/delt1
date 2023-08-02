38c65184fec391893220daae602eb2bd0d59ce90
dijitol77 <saul.a.howells@gmail.com>
2023-08-02 16:06:12 +0100

    Update AudioProcessing.cpp

--- src/AudioProcessing.cpp
+++ src/AudioProcessing.cpp
@@ -138,14 +138,3 @@ void ProteusAudioProcessor::processBlock (AudioBuffer<float>& buffer, MidiBuffer
         }
     }
 }
-
-//==============================================================================
-bool ProteusAudioProcessor::hasEditor() const
-{
-    return true; // (change this to false if you choose to not supply an editor)
-}
-
-AudioProcessorEditor* ProteusAudioProcessor::createEditor()
-{
-    return new ProteusAudioProcessorEditor (*this);
-}
