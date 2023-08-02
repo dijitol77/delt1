bf544a9d93051314e3ec46b5cfa9836fcd2ede2a
dijitol77 <saul.a.howells@gmail.com>
2023-08-02 12:38:42 +0100

    Update StateManagement.cpp

--- src/StateManagement.cpp
+++ src/StateManagement.cpp
@@ -2,10 +2,87 @@
 
 void ProteusAudioProcessor::getStateInformation (MemoryBlock& destData)
 {
-    // getStateInformation implementation...
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+    
+    auto state = treeState.copyState();
+    std::unique_ptr<XmlElement> xml (state.createXml());
+    xml->setAttribute ("fw_state", fw_state);
+    xml->setAttribute("folder", folder.getFullPathName().toStdString());
+    xml->setAttribute("saved_model", saved_model.getFullPathName().toStdString());
+    xml->setAttribute("current_model_index", current_model_index);
+    xml->setAttribute ("cab_state", cab_state);
+    copyXmlToBinary (*xml, destData);
+
 }
 
 void ProteusAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
-    // setStateInformation implementation...
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+
+    std::unique_ptr<juce::XmlElement> xmlState (getXmlFromBinary (data, sizeInBytes));
+
+    if (xmlState.get() != nullptr)
+    {
+        if (xmlState->hasTagName (treeState.state.getType()))
+        {
+            treeState.replaceState (juce::ValueTree::fromXml (*xmlState));
+            fw_state = xmlState->getBoolAttribute ("fw_state");
+            File temp_saved_model = xmlState->getStringAttribute("saved_model");
+            saved_model = temp_saved_model;
+            cab_state = xmlState->getBoolAttribute ("cab_state");
+
+            current_model_index = xmlState->getIntAttribute("current_model_index");
+            File temp = xmlState->getStringAttribute("folder");
+            folder = temp;
+            if (auto* editor = dynamic_cast<ProteusAudioProcessorEditor*> (getActiveEditor()))
+                editor->resetImages();
+
+            if (saved_model.existsAsFile()) {
+                loadConfig(saved_model);
+            }          
+
+        }
+    }
+}
+
+void ProteusAudioProcessor::set_ampEQ(float bass_slider, float mid_slider, float treble_slider)
+{
+    eq4band.setParameters(bass_slider, mid_slider, treble_slider, 0.0f);
+    eq4band2.setParameters(bass_slider, mid_slider, treble_slider, 0.0f);
+}
+
+void ProteusAudioProcessor::loadConfig(File configFile)
+{
+    this->suspendProcessing(true);
+    pauseVolume = 3;
+    String path = configFile.getFullPathName();
+    char_filename = path.toUTF8();
+
+    LSTM.reset();
+    LSTM2.reset();
+
+    LSTM.load_json(char_filename);
+    LSTM2.load_json(char_filename);
+
+    if (LSTM.input_size == 1) {
+        conditioned = false;
+    } else {
+        conditioned = true;
+    }
+
+    //saved_model = configFile;
+    model_loaded = true;
+    this->suspendProcessing(false);
+}
+
+
+
+//==============================================================================
+// This creates new instances of the plugin..
+AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new ProteusAudioProcessor();
 }
