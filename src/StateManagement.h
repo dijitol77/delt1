cf55ef7473ce1e3d592b3b33cfd9458a6cec16d4
dijitol77 <saul.a.howells@gmail.com>
2023-08-03 21:21:08 +0100

    Update StateManagement.h

--- src/StateManagement.h
+++ src/StateManagement.h
@@ -1,13 +1,18 @@
-#pragma once
-#include "PluginProcessor.h"
-#include "juce_core/juce_core.h" // Include this for the MemoryBlock class
 
-class StateManagement {
+#pragma once
+#include "juce_core/juce_core.h" // Include this for the MemoryBlock class
+#include "PluginProcessor.h"  // Include this for the ProteusAudioProcessor class
+
+class StateManagement
+{
 public:
     StateManagement(ProteusAudioProcessor& p) : processor(p) {}
-    void getStateInformation (juce::MemoryBlock& destData);
-    void setStateInformation (const void* data, int sizeInBytes);
+
+    void getStateInformation(juce::MemoryBlock& destData);
+    void setStateInformation(const void* data, int sizeInBytes);
 
 private:
     ProteusAudioProcessor& processor;
 };
+
+
