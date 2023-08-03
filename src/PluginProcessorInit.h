b35bcb20c01e9ea69dfc14503ad1129fd50e3b65
dijitol77 <saul.a.howells@gmail.com>
2023-08-03 02:10:44 +0100

    Update PluginProcessorInit.h

--- src/PluginProcessorInit.h
+++ src/PluginProcessorInit.h
@@ -2,3 +2,26 @@
 
 #include "JuceHeader.h"
 #include "PluginProcessor.h"
+#include "AudioProcessing.h"
+#include "MidiProcessing.h"
+#include "Programs.h"
+#include "StateManagement.h"
+#include "EditorCreation.h"
+#include "UtilityFunctions.h"
+
+class PluginProcessorInit {
+public:
+    PluginProcessorInit(ProteusAudioProcessor& p) : processor(p), 
+        audioProcessing(p), midiProcessing(p), programs(p), 
+        stateManagement(p), editorCreation(p), utilityFunctions(p) {}
+    void initialize();
+
+private:
+    ProteusAudioProcessor& processor;
+    AudioProcessing audioProcessing;
+    MidiProcessing midiProcessing;
+    Programs programs;
+    StateManagement stateManagement;
+    EditorCreation editorCreation;
+    UtilityFunctions utilityFunctions;
+};
