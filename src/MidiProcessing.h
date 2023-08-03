e9b398ccc5c7231ced186424796ce827975a00c3
dijitol77 <saul.a.howells@gmail.com>
2023-08-03 01:46:41 +0100

    Update MidiProcessing.h

--- src/MidiProcessing.h
+++ src/MidiProcessing.h
@@ -1,12 +1,11 @@
 #pragma once
-#include "AudioProcessing.h"
-#include "juce_audio_plugin_client/juce_audio_plugin_client.h" // Include this for the JucePlugin_WantsMidiInput, etc.
-
-class ProteusAudioProcessor;
+#include "PluginProcessor.h"
 
 class MidiProcessing {
 public:
-    bool acceptsMidi() const;
-    bool producesMidi() const;
-    bool isMidiEffect() const;
+    MidiProcessing(ProteusAudioProcessor& p) : processor(p) {}
+    void processMidi(juce::MidiBuffer& midiMessages);
+
+private:
+    ProteusAudioProcessor& processor;
 };
