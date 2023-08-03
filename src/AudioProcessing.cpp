fd4b8b50d888edbb478780eb802ee112c6e0b6b4
dijitol77 <saul.a.howells@gmail.com>
2023-08-03 01:35:49 +0100

    Update AudioProcessing.cpp

--- src/AudioProcessing.cpp
+++ src/AudioProcessing.cpp
@@ -2,109 +2,109 @@
 #include "AudioProcessing.h"
 #include "juce_dsp/juce_dsp.h" // Include this for the dsp::IIR::Coefficients class
 
-void ProteusAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+void AudioProcessing::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
-    *dcBlocker.state = *dsp::IIR::Coefficients<float>::makeHighPass (sampleRate, 35.0f);
+    *processor.dcBlocker.state = *dsp::IIR::Coefficients<float>::makeHighPass (sampleRate, 35.0f);
 
     // prepare resampler for target sample rate: 44.1 kHz
     constexpr double targetSampleRate = 44100.0;
-    resampler.prepareWithTargetSampleRate({ sampleRate, (uint32)samplesPerBlock, 2 }, targetSampleRate);
+    processor.resampler.prepareWithTargetSampleRate({ sampleRate, (uint32)samplesPerBlock, 2 }, targetSampleRate);
 
     dsp::ProcessSpec specMono { sampleRate, static_cast<uint32> (samplesPerBlock), 1 };
     dsp::ProcessSpec spec{ sampleRate, static_cast<uint32> (samplesPerBlock), 2 };
 
-    dcBlocker.prepare (spec); 
+    processor.dcBlocker.prepare (spec); 
 
-    LSTM.reset();
-    LSTM2.reset();
+    processor.LSTM.reset();
+    processor.LSTM2.reset();
 
     // Set up IR
-    cabSimIRa.prepare(spec);
+    processor.cabSimIRa.prepare(spec);
 }
 
-void ProteusAudioProcessor::releaseResources()
+void AudioProcessing::releaseResources()
 {
     // When playback stops, you can use this as an opportunity to free up any
     // spare memory, etc.
 }
 
-void ProteusAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+void AudioProcessing::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
 {
     ScopedNoDenormals noDenormals;
 
-    auto driveValue = static_cast<float> (driveParam->load());
-    auto masterValue = static_cast<float> (masterParam->load());
-    auto bassValue = static_cast<float> (bassParam->load());
-    auto midValue = static_cast<float> (midParam->load());
-    auto trebleValue = static_cast<float> (trebleParam->load());
+    auto driveValue = static_cast<float> (processor.driveParam->load());
+    auto masterValue = static_cast<float> (processor.masterParam->load());
+    auto bassValue = static_cast<float> (processor.bassParam->load());
+    auto midValue = static_cast<float> (processor.midParam->load());
+    auto trebleValue = static_cast<float> (processor.trebleParam->load());
 
     // Setup Audio Data
     const int numSamples = buffer.getNumSamples();
-    const int numInputChannels = getTotalNumInputChannels();
-    const int sampleRate = getSampleRate();
+    const int numInputChannels = processor.getTotalNumInputChannels();
+    const int sampleRate = processor.getSampleRate();
 
     dsp::AudioBlock<float> block(buffer);
     dsp::ProcessContextReplacing<float> context(block);
 
     // Overdrive Pedal ================================================================== 
-    if (fw_state == 1 && model_loaded == true) {
+    if (processor.fw_state == 1 && processor.model_loaded == true) {
         
-        if (conditioned == false) {
+        if (processor.conditioned == false) {
             // Apply ramped changes for gain smoothing
-            if (driveValue == previousDriveValue)
+            if (driveValue == processor.previousDriveValue)
             {
                 buffer.applyGain(driveValue*2.5);
             }
              else {
-                buffer.applyGainRamp(0, (int) buffer.getNumSamples(), previousDriveValue * 2.5, driveValue * 2.5);
-                previousDriveValue = driveValue;
+                buffer.applyGainRamp(0, (int) buffer.getNumSamples(), processor.previousDriveValue * 2.5, driveValue * 2.5);
+                processor.previousDriveValue = driveValue;
             }
-            auto block44k = resampler.processIn(block);
+            auto block44k = processor.resampler.processIn(block);
             for (int ch = 0; ch < buffer.getNumChannels(); ++ch)
             {
                 // Apply LSTM model
                 if (ch == 0) {
-                    LSTM.process(block44k.getChannelPointer(0), block44k.getChannelPointer(0), (int)block44k.getNumSamples());
+                    processor.LSTM.process(block44k.getChannelPointer(0), block44k.getChannelPointer(0), (int)block44k.getNumSamples());
                 }
                 else if (ch == 1) {
-                    LSTM2.process(block44k.getChannelPointer(1), block44k.getChannelPointer(1), (int)block44k.getNumSamples());
+                    processor.LSTM2.process(block44k.getChannelPointer(1), block44k.getChannelPointer(1), (int)block44k.getNumSamples());
                 }
             }
-            resampler.processOut(block44k, block);
+            processor.resampler.processOut(block44k, block);
         } else {
             buffer.applyGain(1.5); // Apply default boost to help sound
             // resample to target sample rate
             
-            auto block44k = resampler.processIn(block);
+            auto block44k = processor.resampler.processIn(block);
             for (int ch = 0; ch < buffer.getNumChannels(); ++ch)
             {
                 // Apply LSTM model
                 if (ch == 0) {
-                    LSTM.process(block44k.getChannelPointer(0), driveValue, block44k.getChannelPointer(0), (int)block44k.getNumSamples());
+                    processor.LSTM.process(block44k.getChannelPointer(0), driveValue, block44k.getChannelPointer(0), (int)block44k.getNumSamples());
                 }
                 else if (ch == 1) {
-                    LSTM2.process(block44k.getChannelPointer(1), driveValue, block44k.getChannelPointer(1), (int)block44k.getNumSamples());
+                    processor.LSTM2.process(block44k.getChannelPointer(1), driveValue, block44k.getChannelPointer(1), (int)block44k.getNumSamples());
                 }
             }
-            resampler.processOut(block44k, block);
+            processor.resampler.processOut(block44k, block);
         }
 
-        dcBlocker.process(context);
+        processor.dcBlocker.process(context);
 
         for (int ch = 0; ch < buffer.getNumChannels(); ++ch)
         {
             // Apply EQ
             if (ch == 0) {
-                eq4band.process(buffer.getReadPointer(0), buffer.getWritePointer(0), midiMessages, numSamples, numInputChannels, sampleRate);
+                processor.eq4band.process(buffer.getReadPointer(0), buffer.getWritePointer(0), midiMessages, numSamples, numInputChannels, sampleRate);
             
             }
             else if (ch == 1) {
-                eq4band2.process(buffer.getReadPointer(1), buffer.getWritePointer(1), midiMessages, numSamples, numInputChannels, sampleRate);
+                processor.eq4band2.process(buffer.getReadPointer(1), buffer.getWritePointer(1), midiMessages, numSamples, numInputChannels, sampleRate);
             }
         }
 
-        if (cab_state == 1) {
-            cabSimIRa.process(context); // Process IR a on channel 0
+        if (processor.cab_state == 1) {
+            processor.cabSimIRa.process(context); // Process IR a on channel 0
             buffer.applyGain(2.0);
         //} else {
         //    buffer.applyGain(0.7);
@@ -112,24 +112,24 @@ void ProteusAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce
 
         // Master Volume 
         // Apply ramped changes for gain smoothing
-        if (masterValue == previousMasterValue)
+        if (masterValue == processor.previousMasterValue)
         {
             buffer.applyGain(masterValue);
         }
         else {
-            buffer.applyGainRamp(0, (int) buffer.getNumSamples(), previousMasterValue, masterValue);
-            previousMasterValue = masterValue;
+            buffer.applyGainRamp(0, (int) buffer.getNumSamples(), processor.previousMasterValue, masterValue);
+            processor.previousMasterValue = masterValue;
         }
 
         // Smooth pop sound when changing models
-        if (pauseVolume > 0) {
-            if (pauseVolume > 2)
+        if (processor.pauseVolume > 0) {
+            if (processor.pauseVolume > 2)
                 buffer.applyGain(0.0);
-            else if (pauseVolume == 2)
+            else if (processor.pauseVolume == 2)
                 buffer.applyGainRamp(0, (int)buffer.getNumSamples(), 0, masterValue / 2);
             else
                 buffer.applyGainRamp(0, (int)buffer.getNumSamples(), masterValue / 2, masterValue);
-            pauseVolume -= 1;
+            processor.pauseVolume -= 1;
         }
     }
 }
