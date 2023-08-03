9aa75973e066c04a5bc56d055ab950ac5834353f
dijitol77 <saul.a.howells@gmail.com>
2023-08-03 05:57:59 +0100

    Update UtilityFunctions.cpp

--- src/UtilityFunctions.cpp
+++ src/UtilityFunctions.cpp
@@ -1,31 +1,26 @@
-#include "PluginProcessor.h" // Include this first
-#include "UtilityFunctions.h" // Then this
-#include "juce_core/juce_core.h" // Include this for the File class
-
-// Remove the first implementation of loadConfig here
+#include "PluginProcessor.h"
+#include "UtilityFunctions.h"
+#include "juce_core/juce_core.h"
 
 UtilityFunctions::UtilityFunctions(ProteusAudioProcessor& p) : processor(p) {}
 
 void UtilityFunctions::loadConfig(juce::File configFile)
 {
-    processor.suspendProcessing(true);
-    processor.pauseVolume = 3;
     juce::String path = configFile.getFullPathName();
     processor.char_filename = path.toUTF8();
 
-    processor.LSTM.reset();
-    processor.LSTM2.reset();
+    processor.resetLSTM();
+    processor.resetLSTM2();
 
-    processor.LSTM.load_json(processor.char_filename);
-    processor.LSTM2.load_json(processor.char_filename);
+    processor.loadLSTM(processor.char_filename);
+    processor.loadLSTM2(processor.char_filename);
 
-    if (processor.LSTM.input_size == 1) {
+    if (processor.LSTM != nullptr && processor.LSTM->input_size == 1) {
         processor.conditioned = false;
     } else {
         processor.conditioned = true;
     }
 
-    //processor.saved_model = configFile;
     processor.model_loaded = true;
-    processor.suspendProcessing(false);
 }
+
