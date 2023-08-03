e1c956ca6927c161933c70b2149030561102fd20
dijitol77 <saul.a.howells@gmail.com>
2023-08-03 05:16:29 +0100

    Update UtilityFunctions.cpp

--- src/UtilityFunctions.cpp
+++ src/UtilityFunctions.cpp
@@ -2,25 +2,7 @@
 #include "UtilityFunctions.h" // Then this
 #include "juce_core/juce_core.h" // Include this for the File class
 
-void UtilityFunctions::loadConfig(juce::File configFile) {
-    // Check if the file exists
-    if (!configFile.exists()) {
-        // Handle the error, e.g., by logging an error message and returning
-        std::cerr << "Error: Config file does not exist: " << configFile.getFullPathName() << std::endl;
-        return;
-    }
-
-    // Check if the file is in the correct format (e.g., if it's a JSON file)
-    if (configFile.getFileExtension() != ".json") {
-        // Handle the error, e.g., by logging an error message and returning
-        std::cerr << "Error: Config file is not a JSON file: " << configFile.getFullPathName() << std::endl;
-        return;
-    }
-
-    // If the file exists and is in the correct format, proceed with loading it
-    // ...
-}
-
+// Remove the first implementation of loadConfig here
 
 UtilityFunctions::UtilityFunctions(ProteusAudioProcessor& p) : processor(p) {}
 
