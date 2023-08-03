7c07bb044c51a21c1c65128a191c757a93ac16a6
dijitol77 <saul.a.howells@gmail.com>
2023-08-03 22:16:08 +0100

    Update StateManagement.h

--- src/StateManagement.h
+++ src/StateManagement.h
@@ -7,8 +7,9 @@ class StateManagement
 public:
     StateManagement(ProteusAudioProcessor& p) : processor(p) {}
 
-    void getStateInformation (juce::MemoryBlock& destData);
-    void setStateInformation (const void* data, int sizeInBytes);
+void getStateInformation (ProteusAudioProcessor& processor, juce::MemoryBlock& destData);
+void setStateInformation (ProteusAudioProcessor& processor, const void* data, int sizeInBytes);
+
 
 private:
     ProteusAudioProcessor& processor;
