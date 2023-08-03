918c3e3a1573cae2a1b3359617b9e76daf441dd7
dijitol77 <saul.a.howells@gmail.com>
2023-08-03 02:10:07 +0100

    Update PluginProcessorInit.cpp

--- src/PluginProcessorInit.cpp
+++ src/PluginProcessorInit.cpp
@@ -1,88 +1,11 @@
-#include "PluginProcessor.h"
-#include "PluginEditor.h"
-#include <juce_audio_processors/juce_audio_processors.h> // for AudioParameterFloat
-
-// In PluginProcessorInit.h
-#include "PluginProcessor.h"
-#include "AudioProcessing.h"
-#include "MidiProcessing.h"
-#include "Programs.h"
-#include "StateManagement.h"
-#include "EditorCreation.h"
-#include "UtilityFunctions.h"
-
-class PluginProcessorInit {
-public:
-    PluginProcessorInit(ProteusAudioProcessor& p) : processor(p) {}
-    void initialize();
-
-private:
-    ProteusAudioProcessor& processor;
-    AudioProcessing audioProcessing;
-    MidiProcessing midiProcessing;
-    Programs programs;
-    StateManagement stateManagement;
-    EditorCreation editorCreation;
-    UtilityFunctions utilityFunctions;
-};
-
-// In PluginProcessorInit.cpp
 #include "PluginProcessorInit.h"
 
 void PluginProcessorInit::initialize() {
-    audioProcessing = AudioProcessing(processor);
-    midiProcessing = MidiProcessing(processor);
-    programs = Programs(processor);
-    stateManagement = StateManagement(processor);
-    editorCreation = EditorCreation(processor);
-    utilityFunctions = UtilityFunctions(processor);
+    audioProcessing.initialize();
+    midiProcessing.initialize();
+    programs.initialize();
+    stateManagement.initialize();
+    editorCreation.initialize();
+    utilityFunctions.initialize();
 }
 
-// In PluginProcessorInit.cpp
-void PluginProcessorInit::initialize() {
-    audioProcessing = AudioProcessing(processor);
-    midiProcessing = MidiProcessing(processor);
-    programs = Programs(processor);
-    stateManagement = StateManagement(processor);
-    editorCreation = EditorCreation(processor);
-    utilityFunctions = UtilityFunctions(processor);
-}
-
-ProteusAudioProcessor::ProteusAudioProcessor()
-#ifndef JucePlugin_PreferredChannelConfigurations
-    : AudioProcessor(BusesProperties()
-#if ! JucePlugin_IsMidiEffect
-#if ! JucePlugin_IsSynth
-        .withInput("Input", AudioChannelSet::stereo(), true)
-#endif
-        .withOutput("Output", AudioChannelSet::stereo(), true)
-#endif
-    ),
-    treeState(*this, nullptr, "PARAMETER", { std::make_unique<AudioParameterFloat>(GAIN_ID, GAIN_NAME, NormalisableRange<float>(0.0f, 1.0f, 0.01f), 0.5f),
-                        std::make_unique<AudioParameterFloat>(BASS_ID, BASS_NAME, NormalisableRange<float>(-8.0f, 8.0f, 0.01f), 0.0f),
-                        std::make_unique<AudioParameterFloat>(MID_ID, MID_NAME, NormalisableRange<float>(-8.0f, 8.0f, 0.01f), 0.0f),
-                        std::make_unique<AudioParameterFloat>(TREBLE_ID, TREBLE_NAME, NormalisableRange<float>(-8.0f, 8.0f, 0.01f), 0.0f),
-                        std::make_unique<AudioParameterFloat>(MASTER_ID, MASTER_NAME, NormalisableRange<float>(0.0f, 1.0f, 0.01f), 0.5) })
-#endif
-{
-    driveParam = treeState.getRawParameterValue (GAIN_ID);
-    masterParam = treeState.getRawParameterValue (MASTER_ID);
-    bassParam = treeState.getRawParameterValue (BASS_ID);
-    midParam = treeState.getRawParameterValue (MID_ID);
-    trebleParam = treeState.getRawParameterValue (TREBLE_ID);
-
-    auto bassValue = static_cast<float> (bassParam->load());
-    auto midValue = static_cast<float> (midParam->load());
-    auto trebleValue = static_cast<float> (trebleParam->load());
-
-    eq4band.setParameters(bassValue, midValue, trebleValue, 0.0);
-    eq4band2.setParameters(bassValue, midValue, trebleValue, 0.0);
-
-    pauseVolume = 3;
-
-    cabSimIRa.load(BinaryData::default_ir_wav, BinaryData::default_ir_wavSize);
-}
-
-ProteusAudioProcessor::~ProteusAudioProcessor()
-{
-}
