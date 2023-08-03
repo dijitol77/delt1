99691f98c1fc93bb15cda269cddca30c94075cb8
dijitol77 <saul.a.howells@gmail.com>
2023-08-03 22:38:51 +0100

    Update StateManagement.cpp

--- src/StateManagement.cpp
+++ src/StateManagement.cpp
@@ -3,38 +3,38 @@
 
 void StateManagement::getStateInformation (juce::MemoryBlock& destData)
 {
-    auto state = this->processor.treeState.copyState();
+    auto state = processor.treeState.copyState();
     std::unique_ptr<juce::XmlElement> xml (state.createXml());
-    xml->setAttribute ("fw_state", this->processor.fw_state);
-    xml->setAttribute("folder", this->processor.folder.getFullPathName().toStdString());
-    xml->setAttribute("saved_model", this->processor.saved_model.getFullPathName().toStdString());
-    xml->setAttribute("current_model_index", this->processor.current_model_index);
-    xml->setAttribute ("cab_state", this->processor.cab_state);
-    juce::copyXmlToBinary (*xml, destData);
+    xml->setAttribute ("fw_state", processor.fw_state);
+    xml->setAttribute("folder", processor.folder.getFullPathName().toStdString());
+    xml->setAttribute("saved_model", processor.saved_model.getFullPathName().toStdString());
+    xml->setAttribute("current_model_index", processor.current_model_index);
+    xml->setAttribute ("cab_state", processor.cab_state);
+    copyXmlToBinary (*xml, destData);
 }
 
 void StateManagement::setStateInformation (const void* data, int sizeInBytes)
 {
-    std::unique_ptr<juce::XmlElement> xmlState (juce::getXmlFromBinary (data, sizeInBytes));
+    std::unique_ptr<juce::XmlElement> xmlState (getXmlFromBinary (data, sizeInBytes));
 
     if (xmlState.get() != nullptr)
     {
-        if (xmlState->hasTagName (this->processor.treeState.state.getType()))
+        if (xmlState->hasTagName (processor.treeState.state.getType()))
         {
-            this->processor.treeState.replaceState (juce::ValueTree::fromXml (*xmlState));
-            this->processor.fw_state = xmlState->getBoolAttribute ("fw_state");
-            juce::File temp_saved_model = xmlState->getStringAttribute("saved_model");
-            this->processor.saved_model = temp_saved_model;
-            this->processor.cab_state = xmlState->getBoolAttribute ("cab_state");
+            processor.treeState.replaceState (ValueTree::fromXml (*xmlState));
+            processor.fw_state = xmlState->getBoolAttribute ("fw_state");
+            File temp_saved_model = xmlState->getStringAttribute("saved_model");
+            processor.saved_model = temp_saved_model;
+            processor.cab_state = xmlState->getBoolAttribute ("cab_state");
 
-            this->processor.current_model_index = xmlState->getIntAttribute("current_model_index");
-            juce::File temp = xmlState->getStringAttribute("folder");
-            this->processor.folder = temp;
-            if (auto* editor = dynamic_cast<ProteusAudioProcessorEditor*> (this->processor.getActiveEditor()))
+            processor.current_model_index = xmlState->getIntAttribute("current_model_index");
+            File temp = xmlState->getStringAttribute("folder");
+            processor.folder = temp;
+            if (auto* editor = dynamic_cast<ProteusAudioProcessorEditor*> (processor.getActiveEditor()))
                 editor->resetImages();
 
-            if (this->processor.saved_model.existsAsFile()) {
-                this->processor.loadConfig(this->processor.saved_model);
+            if (processor.saved_model.existsAsFile()) {
+                processor.loadConfig(processor.saved_model);
             }
          }  
     }  
