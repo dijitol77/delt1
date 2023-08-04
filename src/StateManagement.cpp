3536ec7802f187d1da5f943e330680dc21598196
dijitol77 <saul.a.howells@gmail.com>
2023-08-05 00:33:46 +0100

    Update StateManagement.cpp

--- src/StateManagement.cpp
+++ src/StateManagement.cpp
@@ -17,22 +17,6 @@ void StateManagement::getStateInformation (juce::MemoryBlock& destData)
     copyXmlToBinary (*xml, destData);
 }
 
-void StateManagement::getStateInformation (juce::MemoryBlock& destData)
-{
-    // Use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
-
-    auto state = processor.treeState.copyState();
-    std::unique_ptr<juce::XmlElement> xml (state.createXml());
-    xml->setAttribute ("fw_state", processor.fw_state);
-    xml->setAttribute("folder", processor.folder.getFullPathName().toStdString());
-    xml->setAttribute("saved_model", processor.saved_model.getFullPathName().toStdString());
-    xml->setAttribute("current_model_index", processor.current_model_index);
-    xml->setAttribute ("cab_state", processor.cab_state);
-    copyXmlToBinary (*xml, destData);
-}
-
 void StateManagement::setStateInformation (const void* data, int sizeInBytes)
 {
     // Use this method to restore your parameters from this memory block,
@@ -62,33 +46,4 @@ void StateManagement::setStateInformation (const void* data, int sizeInBytes)
             }          
         }
     }
-}
-
-
-
-void StateManagement::setStateInformation (const void* data, int sizeInBytes)
-{
-    std::unique_ptr<juce::XmlElement> xmlState (juce::XmlElement::getXmlFromBinary (data, sizeInBytes)); // Call as a static method of juce::XmlElement
-
-    if (xmlState.get() != nullptr)
-    {
-        if (xmlState->hasTagName (processor.treeState.state.getType()))
-        {
-            processor.treeState.replaceState (ValueTree::fromXml (*xmlState));
-            processor.fw_state = xmlState->getBoolAttribute ("fw_state");
-            File temp_saved_model = xmlState->getStringAttribute("saved_model");
-            processor.saved_model = temp_saved_model;
-            processor.cab_state = xmlState->getBoolAttribute ("cab_state");
-
-            processor.current_model_index = xmlState->getIntAttribute("current_model_index");
-            File temp = xmlState->getStringAttribute("folder");
-            processor.folder = temp;
-            if (auto* editor = dynamic_cast<ProteusAudioProcessorEditor*> (processor.getActiveEditor()))
-                editor->resetImages();
-
-            if (processor.saved_model.existsAsFile()) {
-                processor.loadConfig(processor.saved_model);
-            }
-         }  
-    }  
 }
