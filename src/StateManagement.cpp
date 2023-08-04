1f4ca3c28fb568fd195db4155f961da6601b9d62
dijitol77 <saul.a.howells@gmail.com>
2023-08-04 19:09:53 +0100

    Update StateManagement.cpp

--- src/StateManagement.cpp
+++ src/StateManagement.cpp
@@ -1,7 +1,6 @@
 #include "StateManagement.h"
 #include "juce_core/juce_core.h" // Include this for the MemoryBlock class
 
-
 void StateManagement::getStateInformation (juce::MemoryBlock& destData)
 {
     auto state = processor.treeState.copyState();
@@ -17,33 +16,6 @@ void StateManagement::getStateInformation (juce::MemoryBlock& destData)
 void StateManagement::setStateInformation (const void* data, int sizeInBytes)
 {
     std::unique_ptr<juce::XmlElement> xmlState (juce::XmlElement::getXmlFromBinary (data, sizeInBytes)); // Call as a static method of juce::XmlElement
-
-    if (xmlState.get() != nullptr)
-    {
-        if (xmlState->hasTagName (processor.treeState.state.getType()))
-        {
-            processor.treeState.replaceState (ValueTree::fromXml (*xmlState));
-            processor.fw_state = xmlState->getBoolAttribute ("fw_state");
-            File temp_saved_model = xmlState->getStringAttribute("saved_model");
-            processor.saved_model = temp_saved_model;
-            processor.cab_state = xmlState->getBoolAttribute ("cab_state");
-
-            processor.current_model_index = xmlState->getIntAttribute("current_model_index");
-            File temp = xmlState->getStringAttribute("folder");
-            processor.folder = temp;
-            if (auto* editor = dynamic_cast<ProteusAudioProcessorEditor*> (processor.getActiveEditor()))
-                editor->resetImages();
-
-            if (processor.saved_model.existsAsFile()) {
-                processor.loadConfig(processor.saved_model);
-            }
-         }  
-    }  
-}
-
-void StateManagement::setStateInformation (const void* data, int sizeInBytes)
-{
-    std::unique_ptr<juce::XmlElement> xmlState (getXmlFromBinary (data, sizeInBytes));
 
     if (xmlState.get() != nullptr)
     {
