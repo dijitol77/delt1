d2148134e595b074408a4231a9fce14c1570594f
dijitol77 <saul.a.howells@gmail.com>
2023-08-05 00:19:14 +0100

    Update Programs.cpp

--- src/Programs.cpp
+++ src/Programs.cpp
@@ -1,5 +1,12 @@
 #include "Programs.h"
 
+Programs::Programs(ProteusAudioProcessor& p) : processor(p) {}
+
+void Programs::initialize(ProteusAudioProcessor& p)
+{
+    // Initialization code goes here
+}
+
 int Programs::getNumPrograms()
 {
     return 1;
@@ -12,12 +19,21 @@ int Programs::getCurrentProgram()
 
 void Programs::setCurrentProgram (int index)
 {
+    // Code to set current program goes here
 }
 
 const juce::String Programs::getProgramName (int index)
 {
     return {};
 }
+
+void Programs::changeProgramName (int index, const juce::String& newName)
+{
+    // Code to change program name goes here
+}
+
+
+
 
 void Programs::changeProgramName (int index, const juce::String& newName)
 {
