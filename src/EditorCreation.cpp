a8983811a1dcdd8fdb92721c2a7b7ddd7ca7d8bf
dijitol77 <saul.a.howells@gmail.com>
2023-08-05 01:40:54 +0100

    Update EditorCreation.cpp

--- src/EditorCreation.cpp
+++ src/EditorCreation.cpp
@@ -2,12 +2,13 @@
 #include "PluginEditor.h"
 #include "juce_audio_processors/juce_audio_processors.h" // Include this for the AudioProcessorEditor class
 
+
 bool EditorCreation::hasEditor() const
 {
-    return true; // (change this to false if you choose to not supply an editor)
+    return true; // return true if your plugin has an editor, false otherwise
 }
 
 juce::AudioProcessorEditor* EditorCreation::createEditor()
 {
-    return new ProteusAudioProcessorEditor (processor);
+    return new ProteusAudioProcessorEditor(processor);
 }
