58c36117c1cb6e8409ddde10e2ada56dc377ad11
dijitol77 <saul.a.howells@gmail.com>
2023-08-06 19:54:32 +0100

    Update StateManagement.h

--- src/StateManagement.h
+++ src/StateManagement.h
@@ -1,10 +1,7 @@
 #pragma once
 
-#include "PluginProcessor.h"
 #include <JuceHeader.h>
-#include "RTNeuralLSTM.h" // Change LSTMClass to RT_LSTM
-
-class ProteusAudioProcessorEditor;
+#include "RT_LSTM.h"
 
 class StateManagement
 {
@@ -12,15 +9,11 @@ public:
     StateManagement();
     ~StateManagement();
 
-    void getStateInformation(MemoryBlock& destData, ValueTree treeState, bool fw_state, File folder, File saved_model, int current_model_index, bool cab_state);
-    void setStateInformation(const void* data, int sizeInBytes, ValueTree& treeState, bool& fw_state, File& folder, File& saved_model, int& current_model_index, bool& cab_state, ProteusAudioProcessorEditor* getActiveEditor);
-
+    void getStateInformation(MemoryBlock& destData, ValueTree treeState, bool fw_state, File folder, File saved_model, int current_model_index, bool cab_state, float bass_slider, float mid_slider, float treble_slider);
+    void setStateInformation(const void* data, int sizeInBytes, ValueTree& treeState, bool& fw_state, File& folder, File& saved_model, int& current_model_index, bool& cab_state, float& bass_slider, float& mid_slider, float& treble_slider, ProteusAudioProcessorEditor* getActiveEditor);
     void set_ampEQ(float bass_slider, float mid_slider, float treble_slider, dsp::IIR::Filter<float>& eq4band, dsp::IIR::Filter<float>& eq4band2);
-
-    void loadConfig(File configFile, bool& conditioned, bool& model_loaded, void (*suspendProcessingFunc)(bool), int& pauseVolume, RT_LSTM& LSTM, RT_LSTM& LSTM2, const char* &char_filename); // pass 'char_filename' as a reference
+    void loadConfig(File configFile, bool& conditioned, bool& model_loaded, void (*suspendProcessingFunc)(bool), int& pauseVolume, RT_LSTM& LSTM, RT_LSTM& LSTM2, const char*& char_filename);
 
 private:
     // Add any private members if needed
 };
-
-
