4e32ee4d47412a2b4b34eeaf124a5ca8cef5c9ec
dijitol77 <saul.a.howells@gmail.com>
2023-08-06 01:14:09 +0100

    Update StateManagement.cpp

--- src/StateManagement.cpp
+++ src/StateManagement.cpp
@@ -1,50 +1,83 @@
+// StateManagement.cpp
+
 #include "StateManagement.h"
-#include "PluginProcessor.h" // Include this here
-#include "juce_core/juce_core.h" // Include this for the MemoryBlock class
 
-void StateManagement::getStateInformation (juce::MemoryBlock& destData)
+StateManagement::StateManagement()
 {
-    // Use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
+    // Constructor implementation if needed
+    // From the original file, there doesn't seem to be any specific initialization for state management.
+}
 
-    auto state = processor.treeState.copyState();
-    std::unique_ptr<juce::XmlElement> xml (state.createXml());
-    xml->setAttribute ("fw_state", processor.fw_state);
-    xml->setAttribute("folder", processor.folder.getFullPathName().toStdString());
-    xml->setAttribute("saved_model", processor.saved_model.getFullPathName().toStdString());
-    xml->setAttribute("current_model_index", processor.current_model_index);
-    xml->setAttribute ("cab_state", processor.cab_state);
+StateManagement::~StateManagement()
+{
+    // Destructor implementation if needed
+    // No specific destructor code was provided in the original file.
+}
+
+void StateManagement::getStateInformation(MemoryBlock& destData)
+{
+    auto state = treeState.copyState();
+    std::unique_ptr<XmlElement> xml (state.createXml());
+    xml->setAttribute ("fw_state", fw_state);
+    xml->setAttribute("folder", folder.getFullPathName().toStdString());
+    xml->setAttribute("saved_model", saved_model.getFullPathName().toStdString());
+    xml->setAttribute("current_model_index", current_model_index);
+    xml->setAttribute ("cab_state", cab_state);
     copyXmlToBinary (*xml, destData);
 }
 
-void StateManagement::setStateInformation (const void* data, int sizeInBytes)
+void StateManagement::setStateInformation(const void* data, int sizeInBytes)
 {
-    // Use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
-
     std::unique_ptr<juce::XmlElement> xmlState (getXmlFromBinary (data, sizeInBytes));
 
     if (xmlState.get() != nullptr)
     {
-        if (xmlState->hasTagName (processor.treeState.state.getType()))
+        if (xmlState->hasTagName (treeState.state.getType()))
         {
-            processor.treeState.replaceState (juce::ValueTree::fromXml (*xmlState));
-            processor.fw_state = xmlState->getBoolAttribute ("fw_state");
-            juce::File temp_saved_model = xmlState->getStringAttribute("saved_model");
-            processor.saved_model = temp_saved_model;
-            processor.cab_state = xmlState->getBoolAttribute ("cab_state");
+            treeState.replaceState (juce::ValueTree::fromXml (*xmlState));
+            fw_state = xmlState->getBoolAttribute ("fw_state");
+            File temp_saved_model = xmlState->getStringAttribute("saved_model");
+            saved_model = temp_saved_model;
+            cab_state = xmlState->getBoolAttribute ("cab_state");
 
-            processor.current_model_index = xmlState->getIntAttribute("current_model_index");
-            juce::File temp = xmlState->getStringAttribute("folder");
-            processor.folder = temp;
-
-            if (auto* editor = dynamic_cast<ProteusAudioProcessorEditor*> (processor.getActiveEditor()))
+            current_model_index = xmlState->getIntAttribute("current_model_index");
+            File temp = xmlState->getStringAttribute("folder");
+            folder = temp;
+            if (auto* editor = dynamic_cast<ProteusAudioProcessorEditor*> (getActiveEditor()))
                 editor->resetImages();
 
-            if (processor.saved_model.existsAsFile()) {
-                processor.loadConfig(processor.saved_model);
+            if (saved_model.existsAsFile()) {
+                loadConfig(saved_model);
             }          
         }
     }
+}
+
+void StateManagement::set_ampEQ(float bass_slider, float mid_slider, float treble_slider)
+{
+    eq4band.setParameters(bass_slider, mid_slider, treble_slider, 0.0f);
+    eq4band2.setParameters(bass_slider, mid_slider, treble_slider, 0.0f);
+}
+
+void StateManagement::loadConfig(File configFile)
+{
+    this->suspendProcessing(true);
+    pauseVolume = 3;
+    String path = configFile.getFullPathName();
+    char_filename = path.toUTF8();
+
+    LSTM.reset();
+    LSTM2.reset();
+
+    LSTM.load_json(char_filename);
+    LSTM2.load_json(char_filename);
+
+    if (LSTM.input_size == 1) {
+        conditioned = false;
+    } else {
+        conditioned = true;
+    }
+
+    model_loaded = true;
+    this->suspendProcessing(false);
 }
