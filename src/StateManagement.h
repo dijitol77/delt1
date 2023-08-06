d380c9fe59915d0db2a188d0308e16be1c9f9d2f
dijitol77 <saul.a.howells@gmail.com>
2023-08-06 01:32:55 +0100

    Update StateManagement.h

--- src/StateManagement.h
+++ src/StateManagement.h
@@ -10,11 +10,13 @@ public:
     StateManagement();
     ~StateManagement();
 
-    void getStateInformation(MemoryBlock& destData);
-    void setStateInformation(const void* data, int sizeInBytes);
-    void set_ampEQ(float bass_slider, float mid_slider, float treble_slider);
-    void loadConfig(File configFile);
+    void getStateInformation(MemoryBlock& destData, ValueTree treeState, /* other necessary variables */);
+    void setStateInformation(const void* data, int sizeInBytes, ValueTree& treeState, /* other necessary variables */);
+    
+    void set_ampEQ(float bass_slider, float mid_slider, float treble_slider, /* any other necessary variables */);
+    void loadConfig(File configFile, /* any other necessary variables */);
 
 private:
     // Add private member variables and methods here if any
 };
+
