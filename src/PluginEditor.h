c1f0b873f369a003048980748fb2fff9b156693c
dijitol77 <saul.a.howells@gmail.com>
2023-08-07 01:27:42 +0100

    Update PluginEditor.h

--- src/PluginEditor.h
+++ src/PluginEditor.h
@@ -18,8 +18,8 @@
 /**
 */
 class ProteusAudioProcessorEditor  : public AudioProcessorEditor,
-                                       private Button::Listener,
-                                       private Slider::Listener                  
+                                     private Button::Listener,
+                                     private Slider::Listener                  
 {
 public:
     ProteusAudioProcessorEditor (ProteusAudioProcessor&);
@@ -38,45 +38,43 @@ private:
     // access the processor object that created it.
     ProteusAudioProcessor& processor;
 
+    // Containers
+    Component originalContainer;
+    Component duplicateContainer;
 
+    // Original Components
     TextButton loadButton;
-    virtual void buttonClicked(Button* button) override;
-
-    bool isValidFormat(File configFile);
-    void loadButtonClicked();
-
-    //Image background = ImageCache::getFromMemory(BinaryData::smart_pedal_jpg, BinaryData::smart_pedal_jpgSize);
-    // LookandFeels and Graphics
-    Image background_on = ImageCache::getFromMemory(BinaryData::background_on_jpg, BinaryData::background_on_jpgSize);
-    Image background_on_blue = ImageCache::getFromMemory(BinaryData::background_on_blue_jpg, BinaryData::background_on_blue_jpgSize);
-    //Image background_off = ImageCache::getFromMemory(BinaryData::background_off_jpg, BinaryData::background_off_jpgSize);
-
-    // Global Widgets
     Label modelLabel;
     Label versionLabel;
-
     ComboBox modelSelect;
-
-    // Overdrive Widgets
     Slider ampBassKnob;
     Slider ampMidKnob;
     Slider ampTrebleKnob;
     Slider odDriveKnob;
     Slider odLevelKnob;
-    //ImageButton odFootSw;
-    //ImageButton odLED;
     ImageButton cabOnButton;
 
-    
+    // Duplicated Components
+    TextButton loadButton2;
+    Label modelLabel2;
+    Label versionLabel2;
+    ComboBox modelSelect2;
+    Slider ampBassKnob2;
+    Slider ampMidKnob2;
+    Slider ampTrebleKnob2;
+    Slider odDriveKnob2;
+    Slider odLevelKnob2;
+    ImageButton cabOnButton2;
+
     // LookandFeels 
-    //myLookAndFeel blackHexKnobLAF;
     myLookAndFeel bigKnobLAF;
     myLookAndFeel smallKnobLAF;
 
+    virtual void buttonClicked(Button* button) override;
     virtual void sliderValueChanged(Slider* slider) override;
 
     AudioProcessorParameter* getParameter(const String& paramId);
- 
+
     void odFootSwClicked();
     void modelSelectChanged();
     void cabOnButtonClicked();
@@ -89,6 +87,13 @@ public:
     std::unique_ptr <AudioProcessorValueTreeState::SliderAttachment> trebleSliderAttach;
     std::unique_ptr <AudioProcessorValueTreeState::SliderAttachment> driveSliderAttach;
     std::unique_ptr <AudioProcessorValueTreeState::SliderAttachment> masterSliderAttach;
- 
+
+    // Duplicated Slider Attachments
+    std::unique_ptr <AudioProcessorValueTreeState::SliderAttachment> bassSliderAttach2;
+    std::unique_ptr <AudioProcessorValueTreeState::SliderAttachment> midSliderAttach2;
+    std::unique_ptr <AudioProcessorValueTreeState::SliderAttachment> trebleSliderAttach2;
+    std::unique_ptr <AudioProcessorValueTreeState::SliderAttachment> driveSliderAttach2;
+    std::unique_ptr <AudioProcessorValueTreeState::SliderAttachment> masterSliderAttach2;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ProteusAudioProcessorEditor)
 };
