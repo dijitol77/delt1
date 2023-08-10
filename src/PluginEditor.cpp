b8032fd208a4e2fca4f953a3c22b9e1692e45b11
dijitol77 <saul.a.howells@gmail.com>
2023-08-10 20:05:37 +0100

    Update PluginEditor.cpp

--- src/PluginEditor.cpp
+++ src/PluginEditor.cpp
@@ -233,7 +233,8 @@ void ProteusAudioProcessorEditor::resized()
   // right bounds
 
   odDriveKnobRight.setBounds(168, 242, 190, 190); // You can adjust these values as needed
-  modelSelectRight.setBounds(52, 11, 400, 28);
+    loadButton.setBounds(186, 48, 120, 24);
+    modelSelectRight.setBounds(52, 11, 400, 28);
 
 }
 
