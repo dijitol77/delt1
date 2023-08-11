22bd94f71c6a879685259d86b8e4f6b0012cd16e
dijitol77 <saul.a.howells@gmail.com>
2023-08-11 14:53:23 +0100

    Update MyFlexBoxWrapper.h

--- src/MyFlexBoxWrapper.h
+++ src/MyFlexBoxWrapper.h
@@ -2,7 +2,7 @@
 #ifndef MY_JUCE_FLEXBOX_WRAPPER_H
 #define MY_JUCE_FLEXBOX_WRAPPER_H
 
-#include "../juce_gui_basics/layout/juce_FlexBox.h"
+#include "./modules/juce_gui_basics/layout/juce_FlexBox.h"
 
 // ... rest of the file ...
 
