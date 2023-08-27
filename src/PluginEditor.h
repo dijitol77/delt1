a1a4a503a6bdf62e45b4bb75d3b5af5f94f1c7e3
dijitol77 <saul.a.howells@gmail.com>
2023-08-27 21:42:55 +0100

    Update PluginEditor.h

--- src/PluginEditor.h
+++ src/PluginEditor.h
@@ -9,73 +9,120 @@
 */
 
 #pragma once
+#include <juce_gui_basics/layout/juce_FlexBox.h>
 
 #include "../JuceLibraryCode/JuceHeader.h"
+
 #include "PluginProcessor.h"
 #include "myLookAndFeel.h"
 
-class GUIContainer : public Component, private Button::Listener, private Slider::Listener
+
+//==============================================================================
+
+class ProteusAudioProcessorEditor  : public AudioProcessorEditor,
+                                       private Button::Listener,
+                                       private Slider::Listener                  
 {
 public:
-    GUIContainer(ProteusAudioProcessor& p);
+    ProteusAudioProcessorEditor (ProteusAudioProcessor&);
+    ~ProteusAudioProcessorEditor();
+
+    //==============================================================================
+    void paint (Graphics&) override;
     void resized() override;
-    void buttonClicked(Button* button) override;
-    void sliderValueChanged(Slider* slider) override;
+    std::unique_ptr<FileChooser> myChooser;
+
+    void loadFromFolder();
+    void resetImages();
 
 private:
-    // Declare all the existing GUI elements here
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    // repsonsive layout
+
+    juce::FlexBox mainFlexBox;
+    juce::FlexBox leftContainerFlexBox;
+    juce::FlexBox rightContainerFlexBox;
+
+    ProteusAudioProcessor& processor;
+
+    // Declare the left container 
+
+    Component leftContainer;
     TextButton loadButton;
+
+    // Declare the right container
+
+    Component rightContainer;   
+    TextButton loadButtonRight;
+
     virtual void buttonClicked(Button* button) override;
 
     bool isValidFormat(File configFile);
+    void loadButtonRightClicked();
     void loadButtonClicked();
+    
 
-    //Image background = ImageCache::getFromMemory(BinaryData::smart_pedal_jpg, BinaryData::smart_pedal_jpgSize);
-    // LookandFeels and Graphics
-    Image background_on = ImageCache::getFromMemory(BinaryData::background_on_jpg, BinaryData::background_on_jpgSize);
-    Image background_on_blue = ImageCache::getFromMemory(BinaryData::background_on_blue_jpg, BinaryData::background_on_blue_jpgSize);
-    //Image background_off = ImageCache::getFromMemory(BinaryData::background_off_jpg, BinaryData::background_off_jpgSize);
+
+ImageComponent leftBackground;  // For displaying the image in the left container
+Image leftBackgroundImage;      // The actual image data for the left container
+
+ImageComponent rightBackground; // For displaying the image in the right container
+Image rightBackgroundImage;     // The actual image data for the right container
+
+
+Image leftBackground_on;
+Image leftBackground_on_blue;
+Image rightBackground_on;
+Image rightBackground_on_blue;
+
+
+
+
+
+    
 
     // Global Widgets
     Label modelLabel;
     Label versionLabel;
 
     ComboBox modelSelect;
+    ComboBox modelSelectRight;
 
     // Overdrive Widgets
     Slider ampBassKnob;
     Slider ampMidKnob;
     Slider ampTrebleKnob;
     Slider odDriveKnob;
+    Slider odDriveKnobRight;
     Slider odLevelKnob;
     //ImageButton odFootSw;
     //ImageButton odLED;
     ImageButton cabOnButton;
 
-    // Attachments
-    std::unique_ptr<AudioProcessorValueTreeState::SliderAttachment> bassSliderAttach;
-    std::unique_ptr<AudioProcessorValueTreeState::SliderAttachment> midSliderAttach;
-    std::unique_ptr<AudioProcessorValueTreeState::SliderAttachment> trebleSliderAttach;
-    std::unique_ptr<AudioProcessorValueTreeState::SliderAttachment> driveSliderAttach;
-    std::unique_ptr<AudioProcessorValueTreeState::SliderAttachment> masterSliderAttach;
+    
+    // LookandFeels 
+    //myLookAndFeel blackHexKnobLAF;
+    myLookAndFeel bigKnobLAF;
+    myLookAndFeel smallKnobLAF;
 
-    ProteusAudioProcessor& processor;
-};
+    virtual void sliderValueChanged(Slider* slider) override;
 
-class ProteusAudioProcessorEditor : public AudioProcessorEditor
-{
+    AudioProcessorParameter* getParameter(const String& paramId);
+ 
+    void odFootSwClicked();
+    void modelSelectChanged();
+    void modelSelectChangedRight();
+    void cabOnButtonClicked();
+
+    bool model_loaded = false;
+
 public:
-    ProteusAudioProcessorEditor(ProteusAudioProcessor&);
-    ~ProteusAudioProcessorEditor();
-
-    void paint(Graphics&) override;
-    void resized() override;
-
-private:
-    ProteusAudioProcessor& processor;
-
-    GUIContainer guiContainer1;
-    GUIContainer guiContainer2;
-
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ProteusAudioProcessorEditor)
+    std::unique_ptr <AudioProcessorValueTreeState::SliderAttachment> bassSliderAttach;
+    std::unique_ptr <AudioProcessorValueTreeState::SliderAttachment> midSliderAttach;
+    std::unique_ptr <AudioProcessorValueTreeState::SliderAttachment> trebleSliderAttach;
+    std::unique_ptr <AudioProcessorValueTreeState::SliderAttachment> driveSliderAttach;
+    std::unique_ptr <AudioProcessorValueTreeState::SliderAttachment> masterSliderAttach;
+ 
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ProteusAudioProcessorEditor)
 };
