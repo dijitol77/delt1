6dbb6b6e1bd6e522657ac400bdd4d7201a3c4fce
dijitol77 <saul.a.howells@gmail.com>
2023-08-27 23:18:45 +0100

    Update PluginEditor.h

--- src/PluginEditor.h
+++ src/PluginEditor.h
@@ -1,13 +1,3 @@
-/*
-  ==============================================================================
-
-    This file was auto-generated!
-
-    It contains the basic framework code for a JUCE plugin editor.
-
-  ==============================================================================
-*/
-
 #pragma once
 
 #include "../JuceLibraryCode/JuceHeader.h"
@@ -18,65 +8,61 @@
 /**
 */
 class ProteusAudioProcessorEditor  : public AudioProcessorEditor,
-                                       private Button::Listener,
-                                       private Slider::Listener                  
+                                     private Button::Listener,
+                                     private Slider::Listener                  
 {
 public:
-    ProteusAudioProcessorEditor (ProteusAudioProcessor&);
-    ~ProteusAudioProcessorEditor();
+    ProteusAudioProcessorEditor (ProteusAudioProcessor& p)
+        : AudioProcessorEditor (&p), processor (p)
+    {
+        // Initialize UI components here
+        addAndMakeVisible(loadButton);
+        addAndMakeVisible(ampBassKnob);
+        addAndMakeVisible(ampMidKnob);
+        // ... (initialize other UI components)
+
+        setSize (400, 300);
+    }
+
+    ~ProteusAudioProcessorEditor() override;
 
     //==============================================================================
     void paint (Graphics&) override;
     void resized() override;
-    std::unique_ptr<FileChooser> myChooser;
 
     void loadFromFolder();
     void resetImages();
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
     ProteusAudioProcessor& processor;
 
-
     TextButton loadButton;
-    virtual void buttonClicked(Button* button) override;
+    void buttonClicked(Button* button) override;
 
     bool isValidFormat(File configFile);
     void loadButtonClicked();
 
-    //Image background = ImageCache::getFromMemory(BinaryData::smart_pedal_jpg, BinaryData::smart_pedal_jpgSize);
-    // LookandFeels and Graphics
     Image background_on = ImageCache::getFromMemory(BinaryData::background_on_jpg, BinaryData::background_on_jpgSize);
     Image background_on_blue = ImageCache::getFromMemory(BinaryData::background_on_blue_jpg, BinaryData::background_on_blue_jpgSize);
-    //Image background_off = ImageCache::getFromMemory(BinaryData::background_off_jpg, BinaryData::background_off_jpgSize);
 
-    // Global Widgets
     Label modelLabel;
     Label versionLabel;
-
     ComboBox modelSelect;
 
-    // Overdrive Widgets
     Slider ampBassKnob;
     Slider ampMidKnob;
     Slider ampTrebleKnob;
     Slider odDriveKnob;
     Slider odLevelKnob;
-    //ImageButton odFootSw;
-    //ImageButton odLED;
     ImageButton cabOnButton;
 
-    
-    // LookandFeels 
-    //myLookAndFeel blackHexKnobLAF;
     myLookAndFeel bigKnobLAF;
     myLookAndFeel smallKnobLAF;
 
-    virtual void sliderValueChanged(Slider* slider) override;
+    void sliderValueChanged(Slider* slider) override;
 
     AudioProcessorParameter* getParameter(const String& paramId);
- 
+
     void odFootSwClicked();
     void modelSelectChanged();
     void cabOnButtonClicked();
@@ -89,6 +75,6 @@ public:
     std::unique_ptr <AudioProcessorValueTreeState::SliderAttachment> trebleSliderAttach;
     std::unique_ptr <AudioProcessorValueTreeState::SliderAttachment> driveSliderAttach;
     std::unique_ptr <AudioProcessorValueTreeState::SliderAttachment> masterSliderAttach;
- 
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ProteusAudioProcessorEditor)
 };
