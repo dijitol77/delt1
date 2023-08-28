a884941ae07fb4339595d6de5e4803d2af9d2e11
dijitol77 <saul.a.howells@gmail.com>
2023-08-28 03:29:18 +0100

    Update PluginEditor.h

--- src/PluginEditor.h
+++ src/PluginEditor.h
@@ -20,6 +20,50 @@ private:
     ComboBox modelSelect;
     Slider odDriveKnob;
     // ... other components
+TextButton loadButton;
+    virtual void buttonClicked(Button* button) override;
+
+    bool isValidFormat(File configFile);
+    void loadButtonClicked();
+
+    //Image background = ImageCache::getFromMemory(BinaryData::smart_pedal_jpg, BinaryData::smart_pedal_jpgSize);
+    // LookandFeels and Graphics
+    Image background_on = ImageCache::getFromMemory(BinaryData::background_on_jpg, BinaryData::background_on_jpgSize);
+    Image background_on_blue = ImageCache::getFromMemory(BinaryData::background_on_blue_jpg, BinaryData::background_on_blue_jpgSize);
+    //Image background_off = ImageCache::getFromMemory(BinaryData::background_off_jpg, BinaryData::background_off_jpgSize);
+
+    // Global Widgets
+    Label modelLabel;
+    Label versionLabel;
+
+    ComboBox modelSelect;
+
+    // Overdrive Widgets
+    Slider ampBassKnob;
+    Slider ampMidKnob;
+    Slider ampTrebleKnob;
+    Slider odDriveKnob;
+    Slider odLevelKnob;
+    //ImageButton odFootSw;
+    //ImageButton odLED;
+    ImageButton cabOnButton;
+
+    
+    // LookandFeels 
+    //myLookAndFeel blackHexKnobLAF;
+    myLookAndFeel bigKnobLAF;
+    myLookAndFeel smallKnobLAF;
+
+    virtual void sliderValueChanged(Slider* slider) override;
+
+    AudioProcessorParameter* getParameter(const String& paramId);
+ 
+    void odFootSwClicked();
+    void modelSelectChanged();
+    void cabOnButtonClicked();
+
+    bool model_loaded = false;
+
 };
 
 class RightContainer : public LeftContainer
