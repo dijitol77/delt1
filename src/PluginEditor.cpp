0e2be0055413359876dd5a5bf77eeb79ffb824b3
dijitol77 <saul.a.howells@gmail.com>
2023-08-28 23:29:04 +0100

    Update PluginEditor.cpp

--- src/PluginEditor.cpp
+++ src/PluginEditor.cpp
@@ -22,11 +22,11 @@ ProteusAudioProcessorEditor::ProteusAudioProcessorEditor (ProteusAudioProcessor&
     addAndMakeVisible(resizableBorder.get());
 
     constrainer.setFixedAspectRatio(700.0 / 200.0);  // Set the fixed aspect ratio
-    setSize(700, 200);  // Set the initial size
+    setSize(175, 50);  // Set the initial size
 
 
     // Set the constrainer's minimum and maximum sizes
-    constrainer.setSizeLimits(500, 650, 2000, 1300);
+    constrainer.setSizeLimits(175, 50, 700, 200);
     setConstrainer(&constrainer);  // Add this line
 
 
