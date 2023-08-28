0f0b8a561d02c88bef5dd8126a1b6b98147d021c
dijitol77 <saul.a.howells@gmail.com>
2023-08-28 03:35:15 +0100

    Update PluginEditor.h

--- src/PluginEditor.h
+++ src/PluginEditor.h
@@ -19,43 +19,30 @@ private:
     TextButton loadButton;
     ComboBox modelSelect;
     Slider odDriveKnob;
-    // ... other components
-TextButton loadButton;
+   
     virtual void buttonClicked(Button* button) override;
 
     bool isValidFormat(File configFile);
     void loadButtonClicked();
 
-    //Image background = ImageCache::getFromMemory(BinaryData::smart_pedal_jpg, BinaryData::smart_pedal_jpgSize);
-    // LookandFeels and Graphics
     Image background_on = ImageCache::getFromMemory(BinaryData::background_on_jpg, BinaryData::background_on_jpgSize);
     Image background_on_blue = ImageCache::getFromMemory(BinaryData::background_on_blue_jpg, BinaryData::background_on_blue_jpgSize);
-    //Image background_off = ImageCache::getFromMemory(BinaryData::background_off_jpg, BinaryData::background_off_jpgSize);
-
-    // Global Widgets
     Label modelLabel;
     Label versionLabel;
 
     ComboBox modelSelect;
 
-    // Overdrive Widgets
     Slider ampBassKnob;
     Slider ampMidKnob;
     Slider ampTrebleKnob;
     Slider odDriveKnob;
     Slider odLevelKnob;
-    //ImageButton odFootSw;
-    //ImageButton odLED;
-    ImageButton cabOnButton;
 
-    
-    // LookandFeels 
-    //myLookAndFeel blackHexKnobLAF;
+    ImageButton cabOnButton;
     myLookAndFeel bigKnobLAF;
     myLookAndFeel smallKnobLAF;
 
     virtual void sliderValueChanged(Slider* slider) override;
-
     AudioProcessorParameter* getParameter(const String& paramId);
  
     void odFootSwClicked();
@@ -64,6 +51,12 @@ TextButton loadButton;
 
     bool model_loaded = false;
 
+public:
+    std::unique_ptr <AudioProcessorValueTreeState::SliderAttachment> bassSliderAttach;
+    std::unique_ptr <AudioProcessorValueTreeState::SliderAttachment> midSliderAttach;
+    std::unique_ptr <AudioProcessorValueTreeState::SliderAttachment> trebleSliderAttach;
+    std::unique_ptr <AudioProcessorValueTreeState::SliderAttachment> driveSliderAttach;
+    std::unique_ptr <AudioProcessorValueTreeState::SliderAttachment> masterSliderAttach;
 };
 
 class RightContainer : public LeftContainer
