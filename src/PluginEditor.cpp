cb61f67fcf44ec92281e08c8055124fbc3e0ceeb
dijitol77 <saul.a.howells@gmail.com>
2023-08-31 00:03:24 +0100

    Update PluginEditor.cpp

--- src/PluginEditor.cpp
+++ src/PluginEditor.cpp
@@ -274,15 +274,14 @@ int modelSelectY = blockB1.getY() + 10;  // Align to the top of blockB3 with a s
 modelSelect.setBounds(modelSelectX, modelSelectY, modelSelectWidth, modelSelectHeight);
 
 
-int odDriveKnobWidth = 90;  // Set the width of the odDriveKnob
-int odDriveKnobHeight = 90;  // Set the height of the odDriveKnob
-
-
+int odDriveKnobWidth = 75;  // Set the width of the odDriveKnob
+int odDriveKnobHeight = 75;  // Set the height of the odDriveKnob
 
 int odDriveKnobX = blockA2.getX() + blockA2.getWidth() - odDriveKnobWidth - 10 - 10 - 8 - 1;  // Current X position
-int odDriveKnobY = blockA2.getY() + 10 + 30 + 4 + 5;  // Move 5px down from the current position
+int odDriveKnobY = blockA2.getY() + 10 + 30 + 4 + 5 + 2;  // Move an additional 2px down from the current position
 
 odDriveKnob.setBounds(odDriveKnobX, odDriveKnobY, odDriveKnobWidth, odDriveKnobHeight);
+
 
 
 
