094cbddf79c9bfb77069b1392ca93bda938ccc23
dijitol77 <saul.a.howells@gmail.com>
2023-08-30 23:00:47 +0100

    Update PluginEditor.cpp

--- src/PluginEditor.cpp
+++ src/PluginEditor.cpp
@@ -279,11 +279,8 @@ int odDriveKnobHeight = 100;  // Set the height of the odDriveKnob
 
 
 
-// Move the odDriveKnob 4 pixels to the right from its current position
-int odDriveKnobX = blockA2.getX() + blockA2.getWidth() - odDriveKnobWidth - 10 - 10 + 10 - 20 + 40 - 20 + 3 - 4 + 2 - 2 + 4;
-
-  // Move the odDriveKnob 2 pixels up from its previous position
-int odDriveKnobY = blockA2.getY() + 10 + 30 + 20 + 3 - 25 + 15 - 10 + 7 - 2 - 2;
+int odDriveKnobX = blockA2.getX() + blockA2.getWidth() - odDriveKnobWidth - 10 - 10 - 8;  // Move 8px to the left
+int odDriveKnobY = blockA2.getY() + 10 + 30;  // Keep the Y position the same
 
 odDriveKnob.setBounds(odDriveKnobX, odDriveKnobY, odDriveKnobWidth, odDriveKnobHeight);
 
