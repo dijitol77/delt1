6a8f1b27302f57d54e5e5547471f9a0fa07f1c77
dijitol77 <saul.a.howells@gmail.com>
2023-08-31 20:19:31 +0100

    git.useEditorAsCommitInput

--- src/PluginEditor.h
+++ src/PluginEditor.h
@@ -3,9 +3,6 @@
 
     This file was auto-generated!
 
-
-
-
     It contains the basic framework code for a JUCE plugin editor.
 
   ==============================================================================
