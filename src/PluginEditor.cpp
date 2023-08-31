f9052bcc1da54305848f5dbe74fb4ca8f7c55d3d
dijitol77 <saul.a.howells@gmail.com>
2023-08-31 18:47:35 +0100

    git add .; git commit --amend --no-edit; git push --force

--- src/PluginEditor.cpp
+++ src/PluginEditor.cpp
@@ -7,7 +7,6 @@
 
   ==============================================================================
 */
-//need this to be quicker and without fannying on ...ss
 
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
