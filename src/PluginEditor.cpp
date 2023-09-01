fa660dd9be0d63818f589c1aa89b57c9782538c8
dijitol77 <saul.a.howells@gmail.com>
2023-09-01 15:45:32 +0100

    Update PluginEditor.cpp

--- src/PluginEditor.cpp
+++ src/PluginEditor.cpp
@@ -275,7 +275,7 @@ void ProteusAudioProcessorEditor::resized()
     modelSelect.setBounds(modelSelectX, modelSelectY, modelSelectWidth, modelSelectHeight);
 
     // Calculate the relative offsets based on blockA2's dimensions
-    int offsetX = blockA2.getWidth() * 0.33;  // 30% of blockA2's width
+    int offsetX = blockA2.getWidth() * 0.40;  // 30% of blockA2's width
     int offsetY = blockA2.getHeight() * 0.33;  // 10% of blockA2's height
 
     // Calculate the size and position for odDriveKnob
