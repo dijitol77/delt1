c320e3e5b6724c31653e2977e29cfcc380a0b46d
dijitol77 <saul.a.howells@gmail.com>
2023-09-05 01:23:13 +0100

    Delete src/MyFlexBoxWrapper.h

--- src/MyFlexBoxWrapper.h
+++
@@ -1,9 +0,0 @@
-// MyFlexBoxWrapper.h
-#ifndef MY_JUCE_FLEXBOX_WRAPPER_H
-#define MY_JUCE_FLEXBOX_WRAPPER_H
-
-#include "JUCE/modules/juce_gui_basics/layout/juce_FlexBox.h"
-
-// ... rest of the file ...
-
-#endif // MY_JUCE_FLEXBOX_WRAPPER_H
